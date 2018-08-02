86e8a220c53c00d0e43d98247bb73cbe4366331f
Axel Kollberg <bollbergo@gmail.com>
2018-08-02 16:52:54 +0200

    Added a template version to the script page, to show for the customer, hopefully a more clear way to visualize the database

--- AngularApp/filmBuilder/src/app/app.module.ts
+++ AngularApp/filmBuilder/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatToolbarModule, MatDividerModule, 
-  MatLineModule, MatProgressSpinnerModule, MatListModule, MatTableModule } from '@angular/material';
+  MatLineModule, MatProgressSpinnerModule, MatListModule, MatTableModule, MatTabsModule, 
+  MatGridListModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -32,14 +33,19 @@ import { StoreService } from "./store.service";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule,
     MatButtonModule,
-    MatToolbarModule,
+    MatCheckboxModule,
     MatDividerModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatInputModule,
     MatLineModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule
